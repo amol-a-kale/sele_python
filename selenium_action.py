27b5837c444d088f574e6b6e69b8bea2e7f96208
amol-a-kale <amolakale111@gmail.com>
2020-03-18 18:41:21 +0530

    add action class program

---
+++ selenium_action.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+from time import sleep
+
+
+from selenium.webdriver.common.by import By
+driver=webdriver.Chrome(executable_path="C:\\Users\\Sanket\\amol2\\webdriver\\chromedriver.exe")
+
+driver.get("https://aiims.edu/en.html")
+
+driver.maximize_window()
+
+sleep(10)
+tenders = driver.find_element(By.XPATH, "//a[@title='Tenders']")
+actions = ActionChains(driver)
+actions.move_to_element(tenders).perform() # when we use action class then use this  perform call operation
+Aiims_tenders = driver.find_element(By.XPATH, "//span/span[text()='AIIMS Tenders']")
+actions.move_to_element(Aiims_tenders).click().perform()
+tender_title=driver.find_elements(By.XPATH, "//td/a")
+sleep(10)
+tender_title_list=[]
+for title in tender_title:
+    title_tender=title.text
+    tender_title_list.append(title_tender)
+print(tender_title_list)
+
+driver.close()
+driver.quit()
